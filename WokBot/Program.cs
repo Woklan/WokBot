bed1eef944d565a170509faf621cd8e7def33834
Lachlan Borwick <lachlanborwick@gmail.com>
2021-06-20 20:37:39 +1000

    Added Event & Command Infrastructure

--- WokBot/Program.cs
+++ WokBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using WokBot.Interfaces;
@@ -12,16 +13,28 @@ namespace WokBot
         public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         private DiscordSocketClient _client;
         public async Task MainAsync() {
-            _client = new DiscordSocketClient();
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
 
             ResourcesInterface result = JsonConvert.DeserializeObject<ResourcesInterface>(File.ReadAllText(@"C:\Users\Wokla\source\repos\WokBot\WokBot\resources.json").Replace('\"', ' '));
-
-            Console.WriteLine("LOGIN");
 
             await _client.LoginAsync(Discord.TokenType.Bot, result.discord);
             await _client.StartAsync();
 
+            _client.MessageUpdated += MessageUpdated;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+
             await Task.Delay(-1);
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
         }
     }
 }
