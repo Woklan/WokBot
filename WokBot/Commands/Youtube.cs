e1448a3bdc762d84e80303ea9e1b29e327ddbdae
Lachlan Borwick <lachlanborwick@gmail.com>
2021-07-05 02:32:06 +1000

    Created Youtube Command & Youtube Interface

---
+++ WokBot/Commands/Youtube.cs
@@ -0,0 +1,114 @@
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using FFmpeg.NET;
+using NYoutubeDL;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using WokBot.Interfaces;
+
+namespace WokBot.Commands
+{
+    public class YoutubeModule : ModuleBase<SocketCommandContext>
+    {
+        private static Random random = new Random();
+        static async Task<MediaFile> ModifyVideo()
+        {
+            Console.WriteLine("FFMPEG");
+            var inputFile = new InputFile(@Program.resourcesInterface.video_output + "video.mp3");
+            var outputFile = new OutputFile(@Program.resourcesInterface.video_output + "video2.mp3");
+            var ffmpeg = new Engine(Program.resourcesInterface.ffmpeg_executable);
+
+            var options = new ConversionOptions();
+
+            options.CutMedia(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(5));
+            var output = await ffmpeg.ConvertAsync(inputFile, outputFile, options);
+
+            return output;
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = Program.resourcesInterface.ffmpeg_executable,
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        [Command("youtube", RunMode = RunMode.Async)]
+        public async Task SayAsync()
+        {
+            try
+            {
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz_";
+                string random_combination = new string(Enumerable.Repeat(chars, 5).Select(s => s[random.Next(s.Length)]).ToArray());
+
+                string url = "https://www.googleapis.com/youtube/v3/search?key=" + Program.resourcesInterface.youtube + "&maxResults=1&part=snippet&type=video&q=" + random_combination;
+
+                youtubeInterface data = await Program.utility.ApiCall<youtubeInterface>(url);
+
+                Console.WriteLine("ApiCall Success");
+
+                string video_id = data.Items[0].Id.VideoId;
+
+                var youtubeDL = new YoutubeDL();
+
+                youtubeDL.Options.FilesystemOptions.Output = Program.resourcesInterface.video_output + "video.mp3";
+                youtubeDL.Options.PostProcessingOptions.ExtractAudio = true;
+                youtubeDL.VideoUrl = "https://www.youtube.com/watch?v=" + video_id;
+
+                youtubeDL.Options.PostProcessingOptions.AudioFormat = NYoutubeDL.Helpers.Enums.AudioFormat.mp3;
+
+                // POGPGOGOGOGOEGPOGPGPOOPEGPPGOOGOG
+
+                youtubeDL.YoutubeDlPath = Program.resourcesInterface.video_executable;
+
+                youtubeDL.StandardOutputEvent += (sender, output) => Console.WriteLine(output);
+                youtubeDL.StandardErrorEvent += (sender, errorOutput) => Console.WriteLine(errorOutput);
+
+                await youtubeDL.DownloadAsync();
+
+                Console.WriteLine("Youtube Success");
+
+                await ModifyVideo();
+
+                IVoiceChannel channel = null;
+
+                channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+
+                if(channel == null)
+                {
+                    return;
+                }
+
+                var audioClient = await channel.ConnectAsync();
+
+                using (var ffmpeg = CreateStream(Program.resourcesInterface.video_output + "video2.mp3"))
+                using (var output = ffmpeg.StandardOutput.BaseStream)
+                using (var discord = audioClient.CreatePCMStream(AudioApplication.Mixed))
+                {
+                    try { await output.CopyToAsync(discord); }
+                    finally { await discord.FlushAsync();  }
+                }
+
+                File.Delete(Program.resourcesInterface.video_output + "video.mp3");
+                File.Delete(Program.resourcesInterface.video_output + "video2.mp3");
+
+
+
+                Console.WriteLine("BIG SUCCESSS");
+            }catch(Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+            
+        }
+    }
+}
---
+++ WokBot/Interfaces/youtube.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace WokBot.Interfaces
+{
+    public class PageInfo
+    {
+        [JsonProperty("totalResults")]
+        public int TotalResults { get; set; }
+
+        [JsonProperty("resultsPerPage")]
+        public int ResultsPerPage { get; set; }
+    }
+
+    public class Id
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("videoId")]
+        public string VideoId { get; set; }
+    }
+
+    public class Default
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+    }
+
+    public class Medium
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+    }
+
+    public class High
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+    }
+
+    public class Thumbnails
+    {
+        [JsonProperty("default")]
+        public Default Default { get; set; }
+
+        [JsonProperty("medium")]
+        public Medium Medium { get; set; }
+
+        [JsonProperty("high")]
+        public High High { get; set; }
+    }
+
+    public class Snippet
+    {
+        [JsonProperty("publishedAt")]
+        public DateTime PublishedAt { get; set; }
+
+        [JsonProperty("channelId")]
+        public string ChannelId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("thumbnails")]
+        public Thumbnails Thumbnails { get; set; }
+
+        [JsonProperty("channelTitle")]
+        public string ChannelTitle { get; set; }
+
+        [JsonProperty("liveBroadcastContent")]
+        public string LiveBroadcastContent { get; set; }
+
+        [JsonProperty("publishTime")]
+        public DateTime PublishTime { get; set; }
+    }
+
+    public class Item
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag")]
+        public string Etag { get; set; }
+
+        [JsonProperty("id")]
+        public Id Id { get; set; }
+
+        [JsonProperty("snippet")]
+        public Snippet Snippet { get; set; }
+    }
+    public class youtubeInterface
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag")]
+        public string Etag { get; set; }
+
+        [JsonProperty("nextPageToken")]
+        public string NextPageToken { get; set; }
+
+        [JsonProperty("regionCode")]
+        public string RegionCode { get; set; }
+
+        [JsonProperty("pageInfo")]
+        public PageInfo PageInfo { get; set; }
+
+        [JsonProperty("items")]
+        public List<Item> Items { get; set; }
+    }
+}
