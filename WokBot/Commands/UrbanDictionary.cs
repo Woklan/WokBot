4d13bf2c45bf5450d6226051268c1dc26929c1f9
Lachlan Borwick <lachlanborwick@gmail.com>
2021-07-04 15:46:16 +1000

    Implemented Urban fully

--- WokBot/Commands/UrbanDictionary.cs
+++ WokBot/Commands/UrbanDictionary.cs
@@ -1,5 +1,5 @@
-﻿using Discord.Commands;
-using System;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 using WokBot.Interfaces;
 
@@ -17,11 +17,19 @@ namespace WokBot.Commands
 
             if (result.data.Count != 0)
             {
+                var embed = new EmbedBuilder();
+                embed.Title = search_term;
+                embed.AddField("Definition", result.data[0].Definition);
+                embed.AddField("Example", result.data[0].Example);
+                embed.WithUrl(result.data[0].Permalink);
+                embed.WithColor(Color.Blue);
+                embed.WithFooter(footer => footer.Text = "Submitted by: " + result.data[0].Author);
+                await ReplyAsync(embed: embed.Build());
                 await Context.Channel.SendMessageAsync("POG!");
             }
             else
             {
-                await Context.Channel.SendMessageAsync("Empty!");
+                await Context.Channel.SendMessageAsync("I found no definition for the term: " + search_term + ".");
             }
         }
     }
