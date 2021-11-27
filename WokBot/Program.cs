8833d891da59447d56ca413873de5f43fd28e8aa
Lachlan Borwick <lachlanborwick@gmail.com>
2021-11-27 17:35:18 +1100

    Implmented Logger

--- WokBot/Program.cs
+++ WokBot/Program.cs
@@ -4,24 +4,36 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using WokBot.Classes;
+using WokBot.Classes.Logging;
 using WokBot.Interfaces;
 
 namespace WokBot
 {
     public class Program
     {
-        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public static void Main(string[] args) => new Program().MainAsync(args).GetAwaiter().GetResult();
         private DiscordSocketClient _client;
         private static CommandHandler commandHandler;
         public static CommandService Commands;
         public static ResourcesInterface resourcesInterface;
         public static Utility utility;
         public static ulong bot_id = 466905552648142848;
-        public async Task MainAsync()
+        public static ILogger logger;
+        private static int logCount = 1;
+
+        public async Task MainAsync(string[] args)
         {
-            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            // Starts the Logger
+            using IHost host = CreateHostBuilder(args).Build();
+            logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation(generateLogNum(), "Logger Started...");
+
+            DiscordSocketConfig _config = new DiscordSocketConfig { MessageCacheSize = 100 };
             _client = new DiscordSocketClient(_config);
 
             Commands = new CommandService();
@@ -43,7 +55,6 @@ namespace WokBot
             await commandHandler.InstallCommandsAsync();
             await _client.StartAsync();
 
-            _client.MessageUpdated += MessageUpdated;
             _client.Ready += () =>
             {
                 Console.WriteLine("Bot is connected!");
@@ -54,10 +65,21 @@ namespace WokBot
 
             await Task.Delay(-1);
         }
-        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+
+        public static int generateLogNum()
         {
-            var message = await before.GetOrDownloadAsync();
-            Console.WriteLine($"{message} -> {after}");
+            logCount++;
+            return logCount - 1;
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(builder =>
+                builder.ClearProviders()
+                .AddLogger(configuration =>
+                {
+                    configuration.LogLevels.Add(LogLevel.Warning, ConsoleColor.Magenta);
+                    configuration.LogLevels.Add(LogLevel.Error, ConsoleColor.Red);
+                }));
     }
 }
