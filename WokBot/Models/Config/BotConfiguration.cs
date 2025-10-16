765651da219a3f69c736ac540d73ccd489876995
lachlanborwick <lachlanborwick@gmail.com>
2025-10-16 22:44:11 +1100

    Extract bot into own class

---
+++ WokBot/Models/Config/BotConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace WokBot.Models.Config
+{
+    public class BotConfiguration
+    {
+        public string DockerIndicator { get; set; }
+        public string GameActivity { get; set; }
+        public string ActivityLink { get; set; }
+        public string DefaultSecretLocation { get; set; }
+    }
+}
